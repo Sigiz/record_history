c80846ede6c2cd65cac17630937d17e7a42f2f7c
Gregório Kusowski <gregorio.kusowski@gmail.com>
2012-07-12 14:22:19 -0300

    Translating tabs to spaces.

--- lib/generators/record_history/templates/alter_record_histories_add_transaction_id.rb
+++ lib/generators/record_history/templates/alter_record_histories_add_transaction_id.rb
@@ -2,7 +2,7 @@ class AlterRecordHistoriesAddTransactionId < ActiveRecord::Migration
   def self.up
     add_column :record_histories, :transaction_id, :decimal
     RecordHistoryModel.all.each do |rec|
-    	rec.transaction_id = rec.created_at.to_f
+      rec.transaction_id = rec.created_at.to_f
       rec.save
     end
     change_column :record_histories, :transaction_id, :decimal, :null => false
--- lib/record_history/record_history_model.rb
+++ lib/record_history/record_history_model.rb
@@ -1,38 +1,38 @@
 require 'base64'
 
 class RecordHistoryModel < ActiveRecord::Base
-	self.table_name = "record_histories"
+  self.table_name = "record_histories"
 
-	belongs_to :item, :polymorphic => true
-	belongs_to :author, :polymorphic => true
-	validates :item_type, :item_id, { :presence => true }
-	validates :old_value_dump, :new_value_dump, :presence => { :unless => Proc.new{|record| record.attr_name.blank?} }
+  belongs_to :item, :polymorphic => true
+  belongs_to :author, :polymorphic => true
+  validates :item_type, :item_id, { :presence => true }
+  validates :old_value_dump, :new_value_dump, :presence => { :unless => Proc.new{|record| record.attr_name.blank?} }
 
-	def old_value
-		self.old_value = nil if self.old_value_dump.nil?
-		self.old_value_dump.nil? ? nil : decode_value(self.old_value_dump)
-	end
+  def old_value
+    self.old_value = nil if self.old_value_dump.nil?
+    self.old_value_dump.nil? ? nil : decode_value(self.old_value_dump)
+  end
 
-	def old_value=(value)
-		self.old_value_dump = encode_value(value)
-	end
+  def old_value=(value)
+    self.old_value_dump = encode_value(value)
+  end
 
-	def new_value
-		self.new_value = nil if self.new_value_dump.nil?
-		decode_value(self.new_value_dump)
-	end
+  def new_value
+    self.new_value = nil if self.new_value_dump.nil?
+    decode_value(self.new_value_dump)
+  end
 
-	def new_value=(value)
-		self.new_value_dump = encode_value(value)
-	end
+  def new_value=(value)
+    self.new_value_dump = encode_value(value)
+  end
 
-	private
+  private
 
-	def encode_value(value)
-		Base64.encode64(Marshal.dump(value))
-	end
+  def encode_value(value)
+    Base64.encode64(Marshal.dump(value))
+  end
 
-	def decode_value(value)
-		Marshal.load(Base64.decode64(value))
-	end
+  def decode_value(value)
+    Marshal.load(Base64.decode64(value))
+  end
 end
